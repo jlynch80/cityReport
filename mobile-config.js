4b9362aeb8fed47e0539958481000f1bde159f0e
Justin Lynch <justin@codergv.org>
2015-09-02 20:47:47 -0500

    updated folder structure

--- mobile-config.js
+++ mobile-config.js
@@ -12,13 +12,13 @@ App.info({
 // Set up resources such as icons and launch screens.
 App.icons({
     'iphone': 'icons/icon-60.png',
-    'iphone_2x': 'icons/icon-60@2x.png',
+    'iphone_2x': 'icons/icon-60@2x.png'
     // ... more screen sizes and platforms ...
 });
 
 App.launchScreens({
     'iphone': 'splash/Default~iphone.png',
-    'iphone_2x': 'splash/Default@2x~iphone.png',
+    'iphone_2x': 'splash/Default@2x~iphone.png'
     // ... more screen sizes and platforms ...
 });
 
